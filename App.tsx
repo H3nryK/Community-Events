e5d6ea451009cf0de88493bc2ad3f37f12951f2f
H3nryK <henrykimani89@gmail.com>
2024-07-04 13:48:28 +0300

    push notifications

--- App.tsx
+++ App.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigation';
+import messaging from '@react-native-firebase/messaging';
 
 export default function App() {
+  async function requestUserPermission() {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }
+
+  const getToken = async () => {
+    const token = await messaging().getToken();
+    console.log('Token = ', token);
+  };
+
+  useEffect(() => {
+    requestUserPermission()
+    getToken()
+  }, []);
+
   return (
     <NavigationContainer>
       <AppNavigator />
